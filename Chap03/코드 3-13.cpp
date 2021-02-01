4ee6176a3a0d7e2caed13f52f379a3865495a954
eooomji <dcs0909@naver.com>
2021-02-01 13:22:23 +0900

    크기 및 타입 변환 함수 사용 예

---
+++ Chap03/코드 3-13.cpp
@@ -0,0 +1,20 @@
+void Mat0p7() {
+	Mat img1 = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	Mat img1f;
+	img1.convertTo(img1f, CV_32FC1);
+
+	unchar data1[] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	Mat mat1(3, 4, CV_8UC1, data1);
+	Mat mat2 = mat1.reshape(0, 1);
+
+	cout << "mat1:\n" << mat1 << endl;
+	cout << "mat2:\n" << mat2 << endl;
+
+	Mat mat3 = Mat::ones(1, 4, CV_8UC1) * 255;
+	mat1.push_back(mat3);
+	cout << "Mat1:\n" << mat1 << endl;
+
+	mat1.resize(6, 100);
+	cout << "mat1:\n" << mat1 << endl;
+}
